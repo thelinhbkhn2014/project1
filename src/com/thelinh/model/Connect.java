58513467b74d24482f95a1a6693d95a415fd285b
Hoang Kien <kienhg96@gmail.com>
2016-11-03 21:32:58 +0700

    fix conn

--- src/com/thelinh/model/Connect.java
+++ src/com/thelinh/model/Connect.java
@@ -17,17 +17,14 @@ public class Connect {
     private static Connection conn;
 
     public static Connection getConnect() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/XayDungVaQuanLyDeThiTracNghiem", "postgres", "1234");
-        } catch (Exception ee) {
-            ee.printStackTrace();
+        if (conn == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/XayDungVaQuanLyDeThiTracNghiem", "postgres", "1234");
+            } catch (Exception ee) {
+                ee.printStackTrace();
+            }
         }
         return conn;
-    }
-    public static void main(String[] args) {
-        if (Connect.getConnect() == null) {
-            System.out.println("Null");
-        }
     }
 }
