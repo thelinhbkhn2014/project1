205bcae73c038bdfc3a2b5463da4ddb73bce1341
thelinhbkhn2014 <20142585@student.hust.edu.vn>
2016-12-15 16:07:36 +0700

    msg

--- src/com/thelinh/model/Connect.java
+++ src/com/thelinh/model/Connect.java
@@ -18,27 +18,12 @@ import javax.swing.JOptionPane;
 public class Connect {
 
     private static Connection conn;
-
-    public static Connection getConnect() {
-        if (conn == null) {
-            try (BufferedReader br = new BufferedReader(new FileReader("config.conf"))) {
-                String user;
-                user = br.readLine();
-                if (user == null) {
-                    JOptionPane.showMessageDialog(null, "Missing user in config.conf");
-                    return null;
-                }
-                String password;
-                password = br.readLine();
-                if (password == null) {
-                    JOptionPane.showMessageDialog(null, "Missing password in config.conf");
-                    return null;
-                }
-                Class.forName("org.postgresql.Driver");
-                conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/XayDungVaQuanLyDeThiTracNghiem", user, password);
-            } catch (Exception ee) {
-                ee.printStackTrace();
-            }
+    public static Connection getConnect(){
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/XayDungVaQuanLyDeThiTracNghiem","postgres","thelinh96");      
+        } catch (Exception ee) {
+            ee.printStackTrace();
         }
         return conn;
     }
